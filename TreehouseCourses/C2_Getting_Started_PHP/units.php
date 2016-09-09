ef51be65c34f2d6faaa4dea8f553bfc8da567e5d
syoustra <steph.you07@gmail.com>
2016-09-09 01:13:09 -0400

    Extra Credit for Unit Converter

--- TreehouseCourses/C2_Getting_Started_PHP/units.php
+++ TreehouseCourses/C2_Getting_Started_PHP/units.php
@@ -10,7 +10,7 @@ echo "Weight: ";
 echo $pounds;
 echo " lb = ";
 echo $kilograms;
-echo "kg";
+echo " kg";
 
 
 // number in miles that we want to convert to kilometers
@@ -24,5 +24,66 @@ echo "Distance: ";
 echo $miles;
 echo " miles = ";
 echo $kilometers;
-echo "km";
+echo " km";
+
+
+// EXTRA CREDIT PART 1
+$kilograms = 140;
+$kg_to_lb = 2.20462;
+$pounds = $kilograms * $kg_to_lb;
+echo "Weight: ";
+echo $kilograms;
+echo " kilograms = ";
+echo $pounds;
+echo " lb";
+
+$kilometers = 2.5;
+$km_to_mile = 0.621371;
+$miles = $kilometers * $km_to_mile;
+echo "Distance: ";
+echo $kilometers;
+echo " kilometers = ";
+echo $miles;
+echo " mi";
+
+
+// EXTRA CREDIT PART 2
+$fahrenheit = 72;
+$fahr_to_cel_AFTER_SUBTRACT = 5/9;
+$celsius = ($fahrenheit - 32) * $fahr_to_cel_AFTER_SUBTRACT;
+echo "Temperature: ";
+echo $fahrenheit;
+echo " degrees Fahrenheit = ";
+echo $celsius;
+echo " degrees C";
+
+$celsius = 72;
+$cel_to_fahr_BEFORE_ADDING = 9/5;
+$fahrenheit = $celsius * $cel_to_fahr_BEFORE_ADDING + 32;
+echo "Temperature: ";
+echo $celsius;
+echo " degrees Celsius = ";
+echo $fahrenheit;
+echo " degrees F";
+
+
+// EXTRA CREDIT PART 3
+$dog = 13;
+$dog_to_human = 7;
+$human = $dog * $dog_to_human;
+echo "Dog Age: ";
+echo $dog;
+echo " years = ";
+echo $human;
+echo " human years";
+
+$human = 13;
+$human_to_dog = 1/7;
+$dog = $human * $human_to_dog;
+echo "Human Age: ";
+echo $human;
+echo " years = ";
+echo $dog;
+echo " dog years";
+
 ?>
