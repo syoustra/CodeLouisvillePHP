3bb0a6428cee0ed9edc92e9755ef01f43ca1f6e4
syoustra <steph.you07@gmail.com>
2016-09-08 23:34:27 -0400

    Adding first two scripts.

---
+++ TreehouseCourses/C2_Getting_Started_PHP/hello.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is our first "Hello World!" script
+ *
+ * @author: Stephanie Youstra
+ */
+ //our first php script
+echo 'Hello World!'; //done
+//echo 'Hello Treehouse!'; //done
+
+//you can make a single line comment as long as you want, even wrapping to a new line, as long as you don't add a hard return
+//This is a second comment line
+
+?>
---
+++ TreehouseCourses/C2_Getting_Started_PHP/numbers.php
@@ -0,0 +1,10 @@
+<?php
+$num_one = 1;
+$num_two = 2;
+$num_three = 3;
+
+var_dump( $num_one );
+var_dump( 1 );
+var_dump( "1" );
+var_dump( $num_one + $num_two - $num_three );
+?>
