26e361bcae2ca31b3bbb411103be39a9bc6ce77d
syoustra <steph.you07@gmail.com>
2016-09-11 21:50:16 -0400

    Comparing strings.

--- TreehouseCourses/C2_PHP_Basics/strings.php
+++ TreehouseCourses/C2_PHP_Basics/strings.php
@@ -1,35 +1,43 @@
 <?php
 $name = 'Stephanie';
 $string_one = "Learning to display \"Hello $name!\" to the screen.\n";
-echo $string_one;
+//echo $string_one;
 
 $string_one = 'Learning to display "Hello ' . $name . '!" to the screen.' . "\n";
-echo $string_one;
+//echo $string_one;
 
 $string_one = 'Learning to display '
 	.'"Hello '
 	. $name
 	. '!" to the screen.'
 	. "\n";
-echo $string_one;
+//echo $string_one;
 
 $string_one = 'Learning to display ';
 $string_one .= '"Hello ';
 $string_one .= $name;
 $string_one .= '!" to the screen.';
-$string_one .= "\n";
-//prepend to a string
-$string_one = 'I am ' . $string_one;
+//$string_one .= "\n";
+	//prepend to a string
+	//$string_one = 'I am ' . $string_one;
 echo $string_one;
 
 
 
+
 $isReady = true;
-var_dump($isReady);
+//var_dump($isReady);
 $isReady = false;
-var_dump($isReady);
+//var_dump($isReady);
 
+//var_dump(1 + "2");
 
+$a = 10;
+$b = '10';
 
+//var_dump($a == $b);
+//var_dump($a === $b);
+
+var_dump($string_one == 'Learning to display "Hello Stephanie!" to the screen.')
 
 ?>
