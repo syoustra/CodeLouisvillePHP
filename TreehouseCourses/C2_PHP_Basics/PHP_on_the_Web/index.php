f84e2b7dd1bb8bf4e50eac523d49055fef70a955
syoustra <steph.you07@gmail.com>
2016-09-12 02:46:11 -0400

    Initial commit for PHP on the Web

---
+++ TreehouseCourses/C2_PHP_Basics/PHP_on_the_Web/index.php
@@ -0,0 +1,50 @@
+<?php 
+$display_name = 'Stephanie A Youstra';
+// $current_year = date('Y');  MY WAY OF DOING COPYRIGHT ... then just echo "$current_year $display_name"; ... ALSO DID <?php echo date('Y') . $display_name;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset=utf-8>
+  	<title>PHP Basics by <?php echo $display_name; ?></title>
+  	<link href="css/style.css" rel="stylesheet" />
+  </head>
+  
+  <body>
+    <div id="wrap">
+        <section class="sidebar text-center">
+          <div class="avatar">
+            <img src="img/logo.png" alt="Alt For Image">
+          </div>
+          <h1><?php echo $display_name; ?></h1>
+          <p>Contact:<br />
+          <a href="mailto:">EMAIL</a></p>
+          <hr />
+          <ul class="social">
+            <li><a href=""><span class="icon twitter"></span></a></li>
+          </ul>
+          <hr />
+          <p>Today: D, d M Y</p>
+        </section>
+        <section class="main">
+          <h1>My First PHP Page</h1>
+    
+          <h2>Unit Conversion</h2>
+    
+          <hr />
+    
+          <h2>Daily Exercise</h2>
+        </section>
+    </div>
+    <section class="footer text-center">
+      &copy; <?php 
+        echo date('Y');
+        echo " " . $display_name . ". "; ?>
+      <?php
+// outputs e.g. 'Last modified: March 04 1998 20:43:59.'
+echo "Last modified: " . date ("F d Y H:i:s.", getlastmod());
+?>
+      
+    </section>
+  </body>
+</html>
