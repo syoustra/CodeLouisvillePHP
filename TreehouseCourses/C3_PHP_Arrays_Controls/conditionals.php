80136b76dfc8538ce19a1fa155896a70b8a474e7
syoustra <steph.you07@gmail.com>
2016-09-13 09:43:31 -0400

    Initial commit of Conditionals and Negation Operators.

---
+++ TreehouseCourses/C3_PHP_Arrays_Controls/conditionals.php
@@ -0,0 +1,57 @@
+<?php
+$a = 0;
+$b = 5;
+
+/*if ($a == $b) {
+} else {
+	echo 'Values are NOT equal.';
+}
+if (($a == $b) == false) {
+	echo 'Values are NOT equal.';
+}
+if ($a <> $b) {
+	echo 'Values are NOT equal.';
+}*/
+/*if ($a != $b) {
+	echo 'Values are NOT equal.';
+}
+if ($a !== $b) {
+	echo 'Values are NOT identical.';
+}*/
+
+if ($a) {
+	echo 'true';
+}
+if (!$a) {
+	echo 'false';
+}
+if ($a == false) {
+	echo 'false';
+}
+if ($a != true) {
+	echo 'false';
+}
+
+
+
+/*if ($a == $b) {
+	echo 'values are equal';
+} elseif ($a < $b) {
+	echo '$a is less than $b';
+} elseif ($a > $b) {
+	echo '$a is greater than $b';
+} else {
+	echo 'values are NOT equal';
+}*/
+
+/*$score = 15;
+if ($score >= 60) {
+	echo 'You completed the level!';
+} elseif ($score <= 30) {
+	echo 'You should practice some more before trying this level again.';
+} else {
+	echo 'Please try again.';
+}*/
+
+
+?>
