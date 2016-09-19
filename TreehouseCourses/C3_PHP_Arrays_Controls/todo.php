c9712b736246ea38e3ff867af0aeeb45e6bcf466
syoustra <steph.you07@gmail.com>
2016-09-18 22:25:02 -0400

    Including list.php to further develop todo.php

--- TreehouseCourses/C3_PHP_Arrays_Controls/todo.php
+++ TreehouseCourses/C3_PHP_Arrays_Controls/todo.php
@@ -1,16 +1,34 @@
 <?php
-$list[] = [
-	'title' => 'Laundry',
-	'priority' => 2,
-	'due' => '',
-	'complete' => true,
-];
-$list[] = [
-	'title' => 'Clean out refrigerator',
-	'priority' => 3,
-	'due' => '07/30/2016',
-	'complete' => false,
-];
-var_dump($list);
-echo $list[0]['title'];
+include 'list.php';
+
+
+foreach ($list as $key => $item) {
+	echo $key . ' = ' . $item['title'] . "<br />\n";
+}
+
+
+echo '<table>';
+echo '<tr>';
+echo '<th>Title</th>';
+echo '<th>Priority</th>';
+echo '<th>Due Date</th>';
+echo '<th>Complete</th>';
+echo '</tr>';
+foreach ($list as $item) {
+	echo '<tr>';
+	echo '<td>' . $item['title'] . "</td>\n";
+	echo '<td>' . $item['priority'] . "</td>\n";
+	echo '<td>' . $item['due'] . "</td>\n";
+	echo '<td>';
+	 if ($item['complete']) {
+	 	echo 'Yes';
+	 } else {
+	 	echo 'No';
+	 }
+	 echo "</td>\n";
+	echo '</tr>';
+}
+
+//var_dump($list);
+//echo $list[0]['title'];
 ?>
