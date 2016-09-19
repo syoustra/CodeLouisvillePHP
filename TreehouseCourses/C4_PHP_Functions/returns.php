9f976d4bd1bc0f4110f885848d3ff70d8e40c5e5
syoustra <steph.you07@gmail.com>
2016-09-19 01:42:39 -0400

    Returns as arrays

---
+++ TreehouseCourses/C4_PHP_Functions/returns.php
@@ -0,0 +1,32 @@
+<?php
+
+function add_up($a, $b) {
+	$arr = array(
+		$a,
+		$b,
+		$a + $b
+	);
+	return $arr;
+}
+
+$value = add_up(2,4);
+
+print_r ($value);
+echo $value[2];
+
+
+// function hello($name) {
+// 	if($name == 'Mike') {
+// 		return 'Hello, Mike!';
+// 	} else {
+// 		return 'Hello, stranger.';
+// 	}
+
+// }
+
+// $greeting = hello('Chris');
+
+// echo $greeting;
+
+
+?>
