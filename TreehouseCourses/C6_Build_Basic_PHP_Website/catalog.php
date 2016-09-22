405e7148b3cdcfad0e8aa3a5f7d0301f6918097c
syoustra <steph.you07@gmail.com>
2016-09-22 19:36:11 -0400

    Using [] to specify subcontent to be shown

--- TreehouseCourses/C6_Build_Basic_PHP_Website/catalog.php
+++ TreehouseCourses/C6_Build_Basic_PHP_Website/catalog.php
@@ -1,10 +1,22 @@
 <?php 
 	$pageTitle = "Full Catalog";
+
+	if (isset($_GET["cat"])) {
+
+		if ($_GET["cat"] == "books") {
+			$pageTitle = "Books";
+		} else if ($_GET["cat"] == "movies") {
+			$pageTitle = "Movies";
+		} else if ($_GET["cat"] == "music") {
+			$pageTitle = "Music";
+		}
+	}
+
 	include("inc/header.php"); 
 ?>
 
 <div class="section page">
-	<h1>Full Catalog</h1>
+	<h1><?php echo $pageTitle; ?></h1>
 </div>
 
 <?php include("inc/footer.php"); ?>
