7a724e9f440443ec8c626f0f59da3a334df916c1
syoustra <steph.you07@gmail.com>
2016-09-22 20:07:36 -0400

    Extra Credit of adding Info page (but no CSS adjustment yet)

---
+++ TreehouseCourses/C6_Build_Basic_PHP_Website/company_info.php
@@ -0,0 +1,12 @@
+<?php 
+	$pageTitle = "Company Information";
+	$section = "info";
+
+	include("inc/header.php"); 
+?>
+
+<div class="section page">
+	<h1>Company Information</h1>
+</div>
+
+<?php include("inc/footer.php"); ?>
