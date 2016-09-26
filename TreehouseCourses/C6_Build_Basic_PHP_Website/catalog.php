f575c7967d38295288b66a396356bbdb3fba0607
syoustra <steph.you07@gmail.com>
2016-09-26 12:27:49 -0400

    Creating the catalog array

--- TreehouseCourses/C6_Build_Basic_PHP_Website/catalog.php
+++ TreehouseCourses/C6_Build_Basic_PHP_Website/catalog.php
@@ -1,4 +1,10 @@
 <?php 
+$catalog = array(
+	"Design Patterns", 
+	"Forrest Gump", 
+	"Beethoven"
+);
+
 	$pageTitle = "Full Catalog"; // default for when not otherwise specified
 	$section = null; // default for when not otherwise specified
 
@@ -19,8 +25,25 @@
 	include("inc/header.php"); 
 ?>
 
-<div class="section page">
-	<h1><?php echo $pageTitle; ?></h1>
+<div class="section catalog page">
+	<div class="wrapper">
+
+		<h1><?php echo $pageTitle; ?></h1>
+
+		<ul>
+<!-- 			<li>Item 1</li>
+			<li>Item 2</li>
+			<li>Item 3</li> -->
+			<?php 
+			foreach($catalog as $item) {
+				echo "<li>" . $item . "</li>";
+			}
+			?>
+		</ul>
+
+
+	</div>
+
 </div>
 
 <?php include("inc/footer.php"); ?>
