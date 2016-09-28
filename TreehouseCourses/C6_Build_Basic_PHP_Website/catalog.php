1a6124eff476547c007d5a6208481e18601f1a9f
syoustra <steph.you07@gmail.com>
2016-09-28 10:57:21 -0400

    Creating separate file to hold catalog items

--- TreehouseCourses/C6_Build_Basic_PHP_Website/catalog.php
+++ TreehouseCourses/C6_Build_Basic_PHP_Website/catalog.php
@@ -1,9 +1,5 @@
 <?php 
-$catalog = array();
-	$catalog[101] = "Design Patterns";
-	$catalog[201] = "Forrest Gump";
-	$catalog[301] = "Beethoven";
-	$catalog[102] = "Clean Code";
+
 
 
 	$pageTitle = "Full Catalog"; // default for when not otherwise specified
---
+++ TreehouseCourses/C6_Build_Basic_PHP_Website/data.php
@@ -0,0 +1,27 @@
+<?php
+
+$catalog = [];
+	$catalog[101] = [
+		"title" => "Design Patterns",
+		"img" => "img/media/design_patterns.jpg",
+		"genre" => "Tech",
+		"format" => "Paperback",
+		"year" => 1994,
+		"category" => "Books",
+		"isbn" => "978*0201633610",
+		"publisher" => "Prentice Hall",
+		"authors" => [
+			"Erich Gamma",
+			"Richard Helm",
+			"Ralph Johnson",
+			"John Vlissides"
+		]
+	];
+
+
+	$catalog[201] = "Forrest Gump";
+	$catalog[301] = "Beethoven";
+	$catalog[102] = "Clean Code";
+
+
+?>
