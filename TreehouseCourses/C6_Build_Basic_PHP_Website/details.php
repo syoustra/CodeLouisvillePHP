380268d98cdadca46ecf549ba970b63d18b28f67
syoustra <steph.you07@gmail.com>
2016-10-04 02:08:16 -0400

    Setting up and customizing the details page

--- TreehouseCourses/C6_Build_Basic_PHP_Website/details.php
+++ TreehouseCourses/C6_Build_Basic_PHP_Website/details.php
@@ -29,6 +29,71 @@ include("inc/functions.php");
 		<span>		
 		<img src="<?php echo $item["img"]; ?>" alt="<?php echo $item["title"]; ?>" />
 		</span>
+
+		</div>
+
+		<div class="media-details">
+			<h1><?php echo $item["title"]; ?></h1>
+			<table>
+
+				<tr>
+					<th>Category</th>
+					<td><?php echo $item["category"]; ?></td>
+				</tr>
+				<tr>
+					<th>Genre</th>
+					<td><?php echo $item["genre"]; ?></td>
+				</tr>
+				<tr>
+					<th>Format</th>
+					<td><?php echo $item["format"]; ?></td>
+				</tr>
+				<tr>
+					<th>Year</th>
+					<td><?php echo $item["year"]; ?></td>
+				</tr>
+
+				<?php if (strtolower($item["category"]) == "books") {	?>
+					<tr>
+						<th>Authors</th>
+						<td><?php echo implode(", ", $item["authors"]); ?></td>
+					</tr>
+					<tr>
+						<th>Publisher</th>
+						<td><?php echo $item["publisher"]; ?></td>
+					</tr>
+					<tr>
+						<th>ISBN</th>
+						<td><?php echo $item["isbn"]; ?></td>
+					</tr>
+
+					<?php } else if (strtolower($item["category"]) == "movies") {	?>
+					<tr>
+						<th>Director</th>
+						<td><?php echo $item["director"]; ?></td>
+					</tr>
+					<tr>
+						<th>Writers</th>
+						<td><?php echo implode(", ", $item["writers"]); ?></td>
+					</tr>
+					<tr>
+						<th>Stars</th>
+						<td><?php echo implode(", ", $item["stars"]); ?></td>
+					</tr>
+
+					<?php } else if (strtolower($item["category"]) == "music") {	?>
+					<tr>
+						<th>Artist</th>
+						<td><?php echo $item["artist"]; ?></td>
+					</tr>
+
+
+					<?php } ?>
+
+
+			</table>
+
+
 		</div>
 
 	</div>
--- TreehouseCourses/C6_Build_Basic_PHP_Website/inc/functions.php
+++ TreehouseCourses/C6_Build_Basic_PHP_Website/inc/functions.php
@@ -1,14 +1,16 @@
 <?php
 
-function get_item_html($id, $item) {
-	$output = "<li><a href='#'><img src='"
-		. $item["img"] . "' alt='"
-		. $item["title"] . "' />"
-		. "<p>View Details</p>"
-		. "</a></li>";
-		return $output;
+function get_item_html($id,$item) {
+  $output = "<li><a href='details.php?id="
+    . $id . "'><img src='" 
+    . $item["img"] . "' alt='" 
+    . $item["title"] . "' />" 
+    . "<p>View Details</p>"
+    . "</a></li>";
+  return $output;
 }
 
+	
 function array_category($catalog, $category) {
 
 	$output = array();
