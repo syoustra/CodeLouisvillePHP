ac5dff2ae67d1f342b256193418a547358b6a30a
syoustra <steph.you07@gmail.com>
2016-10-03 22:27:05 -0400

    Randomizing main index page

--- TreehouseCourses/C6_Build_Basic_PHP_Website/index.php
+++ TreehouseCourses/C6_Build_Basic_PHP_Website/index.php
@@ -16,8 +16,10 @@
 
 				<ul class="items">
 					<?php 
-					foreach($catalog as $id => $item) {
-						echo get_item_html($id, $item);
+					$random = array_rand($catalog, 4);
+
+					foreach($random as $id) {
+						echo get_item_html($id, $catalog[$id]);
 					}
 					?>
 
