403178f3ffa16d1cd86ec41d025c3e96f4a3d7f8
syoustra <steph.you07@gmail.com>
2016-10-10 11:45:49 -0400

    Adjusting recipe display method

--- TreehouseCourses/C8_Object_Oriented_PHP/classes/render.php
+++ TreehouseCourses/C8_Object_Oriented_PHP/classes/render.php
@@ -5,7 +5,22 @@ class Render
 
 	public static function displayRecipe($recipe)
 	{
-		return $recipe->getTitle() . " by " . $recipe->getSource();
+		$output = "";
+		$output .= $recipe->getTitle() . " by " . $recipe->getSource();
+		$output .= "\n";
+		$output .= implode(", ", $recipe->getTags());
+		$output .= "\n\n";
+		foreach ($recipe->getIngredients() as $ing) {
+			$output .= $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
+			$output .= "\n";
+		}
+		$output .= "\n";
+		$output .= implode("\n", $recipe->getInstructions());
+		$output .= "\n";
+		$output .= $recipe->getYield();
+
+		return $output;
+
 	}
 
 }
