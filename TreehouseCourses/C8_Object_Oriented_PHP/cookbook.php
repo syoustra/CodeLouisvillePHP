658123c7d28a7220ed33d8e9539d41118a36d2af
syoustra <steph.you07@gmail.com>
2016-10-10 13:07:04 -0400

    Magic methods and constants

--- TreehouseCourses/C8_Object_Oriented_PHP/cookbook.php
+++ TreehouseCourses/C8_Object_Oriented_PHP/cookbook.php
@@ -2,24 +2,7 @@
 
 include "classes/recipe.php";
 include "classes/render.php";
-
-$recipe1 = new Recipe("my first recipe");
-$recipe1->setSource = "Grandma Holligan";
-$recipe1->addIngredient("egg", 1);
-$recipe1->addIngredient("flour", 2, "cup");
+include "inc/recipes.php";
 
 
-$recipe2 = new Recipe("my second recipe");
-$recipe2->setSource = "Betty Crocker";
-
-$recipe1->addInstruction("This is my first instruction.");
-$recipe1->addInstruction("This is my second instruction.");
-
-$recipe1->addTag("Breakfast");
-$recipe1->addTag("Main Course");
-
-$recipe1->setYield("6 servings");
-
-echo $recipe1;
-echo new Render();
-//echo Render::displayRecipe($recipe1);
+echo Render::displayRecipe($belgian_waffles);
